27b0c3c6c26f5926c8bf48122627a14e19ba02de
Bernhard Bablok <bablokb@gmx.de>
2018-09-27 16:44:52 +0200

    initial commit

---
+++ rtc_ds3231_template/main.py
@@ -0,0 +1,25 @@
+# -------------------------------------------------------------------------
+# Dump memory information
+#
+# Author: Bernhard Bablok
+# License: GPL3
+#
+# Website: https://github.com/bablokb/trinket-m0
+#
+# -------------------------------------------------------------------------
+
+import time
+import gc
+
+print("free: %s" % gc.mem_free())
+
+import board
+import busio as io
+import adafruit_ds3231
+
+i2c = io.I2C(board.SCL, board.SDA)
+rtc = adafruit_ds3231.DS3231(i2c)
+print("free: %s" % gc.mem_free())
+
+t = rtc.datetime
+print("current time: %02d:%02d:%02d" % (t.tm_hour,t.tm_min, t.tm_sec))
