a251303cf7795963bb51c9a3d2d1694ac84d0261
Bernhard Bablok <bablokb@gmx.de>
2022-05-28 15:08:22 +0200

    added header, reformatted, cleanup

--- esp-01/main.py
+++ esp-01/main.py
@@ -1,25 +1,34 @@
+# -------------------------------------------------------------------------
+# Update on-chip RTC using an ESP-01 as coprocessor.
+#
+# Adapted from sample esp_atcontrol_localtime.py for the pico.
+#
+# Author: Bernhard Bablok
+# License: GPL3
+#
+# Website: https://github.com/bablokb/circuitpython-examples
+#
+# -------------------------------------------------------------------------
+
 # SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
 # SPDX-License-Identifier: MIT
 
 import time
 import board
 import busio
-from digitalio import DigitalInOut
-from digitalio import Direction
 import rtc
 
 from adafruit_espatcontrol import (
-    adafruit_espatcontrol,
-    adafruit_espatcontrol_wifimanager,
+  adafruit_espatcontrol,
+  adafruit_espatcontrol_wifimanager,
 )
-
 
 # Get wifi details and more from a secrets.py file
 try:
-    from secrets import secrets
+  from secrets import secrets
 except ImportError:
-    print("WiFi secrets are kept in secrets.py, please add them there!")
-    raise
+  print("WiFi secrets are kept in secrets.py, please add them there!")
+  raise
 
 # Debug Level
 # Change the Debug Flag if you have issues with AT commands
@@ -31,34 +40,31 @@ sleep_duration = 60
 PIN_RX = board.GP1
 PIN_TX = board.GP0
 uart = busio.UART(PIN_TX, PIN_RX, baudrate=11520, receiver_buffer_size=2048)
-esp_boot = DigitalInOut(board.LED)
-esp_boot.direction = Direction.OUTPUT
-esp_boot.value = True
 status_light = None
 
 print("ESP AT commands")
 esp = adafruit_espatcontrol.ESP_ATcontrol(
-    uart, 115200, reset_pin=None, rts_pin=None, debug=debugflag
+  uart, 115200, reset_pin=None, rts_pin=None, debug=debugflag
 )
-wifi = adafruit_espatcontrol_wifimanager.ESPAT_WiFiManager(esp, secrets, status_light)
+wifi = adafruit_espatcontrol_wifimanager.ESPAT_WiFiManager(
+  esp, secrets, status_light
+)
 
-
-print("ESP32 local time")
+print("ESP-01 local time")
 
 TIME_API = "http://worldtimeapi.org/api/ip"
-
 
 the_rtc = rtc.RTC()
 
 response = None
 while True:
-    try:
-        print("Fetching json from", TIME_API)
-        response = wifi.get(TIME_API)
-        break
-    except (ValueError, RuntimeError, adafruit_espatcontrol.OKError) as e:
-        print("Failed to get data, retrying\n", e)
-        continue
+  try:
+    print("Fetching json from", TIME_API)
+    response = wifi.get(TIME_API)
+    break
+  except (ValueError, RuntimeError, adafruit_espatcontrol.OKError) as e:
+    print("Failed to get data, retrying\n", e)
+    continue
 
 json = response.json()
 current_time = json["datetime"]
@@ -73,12 +79,12 @@ week_day = json["day_of_week"]
 is_dst = json["dst"]
 
 now = time.struct_time(
-    (year, month, mday, hours, minutes, seconds, week_day, year_day, is_dst)
+  (year, month, mday, hours, minutes, seconds, week_day, year_day, is_dst)
 )
 print(now)
 the_rtc.datetime = now
 
 while True:
-    print(time.localtime())
-    print("Sleeping for: {0} Seconds".format(sleep_duration))
-    time.sleep(sleep_duration)
+  print(time.localtime())
+  print("Sleeping for: {0} Seconds".format(sleep_duration))
+  time.sleep(sleep_duration)
