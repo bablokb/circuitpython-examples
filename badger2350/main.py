bfb59157c5e76c249b3f618c9a844e7dfd5ea839
Bernhard Bablok <bablokb@gmx.de>
2026-03-24 16:04:19 +0100

    support builtin display

--- badger2350/main.py
+++ badger2350/main.py
@@ -14,70 +14,103 @@
 
 import time
 import board
-import busio
 import displayio
-import fourwire
 
-#from magtag_ssd1680 import SSD1680
-from badger2350_ssd1680 import SSD1680
-#from adafruit_ssd1680 import SSD1680
-#from pimoroni_ssd1680 import SSD1680
-#from waveshare_ssd1680 import SSD1680
-#from wsalt_ssd1680 import SSD1680
+# --- create display (if not builtin)   --------------------------------------
+
+def get_display():
+  """ check for builtin display, create otherwise """
+
+  if hasattr(board,"DISPLAY"):
+    print("using builtin display")
+    return board.DISPLAY
+
+  import busio
+  import fourwire
+  #from magtag_ssd1680 import SSD1680
+  from badger2350_ssd1680 import SSD1680
+  #from adafruit_ssd1680 import SSD1680
+  #from pimoroni_ssd1680 import SSD1680
+  #from waveshare_ssd1680 import SSD1680
+  #from wsalt_ssd1680 import SSD1680
+
+  print("releasing displays")
+  displayio.release_displays()
+  print("done")
+
+  # pinout for Badger2350 display
+
+  SCK_PIN  = board.SCK
+  MOSI_PIN = board.MOSI
+  MISO_PIN = None
+  CS_PIN   = board.INKY_CS
+  RST_PIN  = board.INKY_RST
+  DC_PIN   = board.INKY_DC
+  BUSY_PIN = board.INKY_BUSY
+
+  spi = busio.SPI(SCK_PIN,MOSI=MOSI_PIN,MISO=MISO_PIN)
+  display_bus = fourwire.FourWire(
+    spi, command=DC_PIN, chip_select=CS_PIN, reset=RST_PIN,
+    baudrate=12000000
+  )
+
+  print("creating display...")
+  return SSD1680(display_bus,
+                    busy_pin=BUSY_PIN,
+                    rotation=270)
+
+
+# --- display image   --------------------------------------------------------
+
+def show_image():
+  """ show image """
+
+  print("opening image...")
+  with open("/display-ruler.bmp", "rb") as f:
+    pic = displayio.OnDiskBitmap(f)
+    t = displayio.TileGrid(pic, pixel_shader=pic.pixel_shader)
+    g.append(t)
+
+    print("refreshing display...")
+    start = time.monotonic()
+    display.refresh()
+    print("...done")
+    print("waiting for busy...")
+    while display.busy:
+      time.sleep(0.05)
+    print(f"...finished after {time.monotonic()-start:0.3f}s")
+
+# --- main program   ---------------------------------------------------------
 
 time.sleep(5)
-print("releasing displays")
-displayio.release_displays()
-print("done")
+if hasattr(board,'RESET_STATE'):
+  print(f"buttons turned on at reset: {board.RESET_STATE():#012b}")
+  if board.ON_RESET_PRESSED(board.SW_A):
+    print("SW_A pressed!")
+  if board.ON_RESET_PRESSED(board.SW_B):
+    print("SW_B pressed!")
+  if board.ON_RESET_PRESSED(board.SW_C):
+    print("SW_C pressed!")
+  if board.ON_RESET_PRESSED(board.SW_DOWN):
+    print("SW_DOWN pressed!")
+  if board.ON_RESET_PRESSED(board.SW_UP):
+    print("SW_UP pressed!")
+  time.sleep(5)
 
-# pinout for Badger2350 display
-
-SCK_PIN  = board.SCK
-MOSI_PIN = board.MOSI
-MISO_PIN = None
-CS_PIN   = board.INKY_CS
-RST_PIN  = board.INKY_RST
-DC_PIN   = board.INKY_DC
-BUSY_PIN = board.INKY_BUSY
-
-spi = busio.SPI(SCK_PIN,MOSI=MOSI_PIN,MISO=MISO_PIN)
-display_bus = fourwire.FourWire(
-  spi, command=DC_PIN, chip_select=CS_PIN, reset=RST_PIN,
-  baudrate=12000000
-)
-
-print("creating display...")
-display = SSD1680(display_bus,
-                  busy_pin=BUSY_PIN,
-                  rotation=270)
-
+display = get_display()
 g = displayio.Group()
 display.root_group = g
 
-print("opening image...")
-with open("/display-ruler.bmp", "rb") as f:
-  pic = displayio.OnDiskBitmap(f)
-  t = displayio.TileGrid(pic, pixel_shader=pic.pixel_shader)
-  g.append(t)
+show_image()
+if hasattr(board,"DISPLAY"):
+  for speed in [board.display.SPEED_SLOW, board.display.SPEED_FAST,
+                board.display.SPEED_FASTER, board.display.SPEED_FASTEST]:
+    time.sleep(5)
+    print(f"changing speed to {speed}...")
+    board.display.SET_UPDATE_SPEED(speed)
+    show_image()
+else:
+  displayio.release_displays()
 
-  print("refreshing display...")
-  display.refresh()
-  print("...done")
-  while display.busy:
-    print("busy...")
-    time.sleep(1)
-  print("...finished")
-
-  #   time.sleep(4)
-  #   print("rotating display...")
-  #   display.rotation = 270
-  #   display.refresh()
-  #   print("...done")
-  #   while display.busy:
-  #     print("busy...")
-  #     time.sleep(1)
-  #   print("...finished")
-
-displayio.release_displays()
 while True:
   time.sleep(120)
